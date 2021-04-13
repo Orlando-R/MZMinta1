f2af7898b1c6537169b38c4ad77816c89788c280
Orlando-R <roglorand@gmail.com>
2021-04-13 22:22:57 +0200

    ötödik feltöltés -kiirás elött

--- MZMinta1/Program.cs
+++ MZMinta1/Program.cs
@@ -12,6 +12,7 @@ namespace MZMinta1
         class Auto 
         {
             public int ertek;
+            public string tipus;
         }
         static void Main(string[] args)
         {
@@ -42,13 +43,46 @@ namespace MZMinta1
             Console.WriteLine($"a legnagyobb: {autok[maxIndex]}) : a legkisebb:{autok[minIndex]}");
 
 
-            //van e egy valamilyen
+            //van e egy valamilyen -eldöntés 1- negálás
+            int i = 0;
+            while(i < N && !(T))
+            {
+                i++;
+            }
+            bool van = i < N;
 
-            //mindenki valamilyen e
+            //mindenki valamilyen e -eldöntés 2 -nincs negálás
+            i = 0;
+            while (i < N && (T))
+            {
+                i++;
+            }
+            bool mind = i >= N;
 
-            //hányféle van
 
-            //melyikből mennyi van
+
+            //hányféle van -HashSet<> szétválogatás; melyek ezek, hányféle érték a listában
+            HashSet<int> ertekek = new HashSet<int>();
+            foreach (Auto auto in autok)
+            {
+                ertekek.Add(auto.ertek);
+            }
+            
+
+            //melyikből mennyi van -Dictionary<string, int> melyik tipusból hány darab a listában
+            Dictionary<string, int> tipusDb = new Dictionary<string, int>();
+            foreach (Auto auto in autok)
+            {
+                string kulcs = auto.tipus;
+                if (tipusDb.ContainsKey(kulcs))
+                {
+                    tipusDb[kulcs]++;
+                }
+                else
+                {
+                    tipusDb.Add(kulcs, 1);
+                }
+            }
 
             //filekiirás
         }
